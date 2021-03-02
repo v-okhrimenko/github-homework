d13def4c371b7273da5ef211218fa4856ebe2abf
Vasyl Okhrimenko <v.okhrimenko@gmail.com>
2021-03-03 00:10:40 +0200

    [HW-02] added metod print in Main.java and update README.md

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a training repository
 1. add .idea to git ignore
 2. just some text...
 3. added Main.java with main method with output to the console "Hello, world!"
+4. added a static print method that takes one parameter - string and that will be printed to the console
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        if (args.length == 1) {
+            print(args[0]);
+        } else {
+            System.out.println("Invalid number of arguments. Enter 2 arguments: first - text, second - how many " +
+                    "times text output to the console ");
+        }
+    }
+
+    static void print(String txt) {
+        System.out.println(txt);
     }
 }
