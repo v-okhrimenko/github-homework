8a563da3b0ae838fc173209df6ac579ef8b9907c
Vasyl Okhrimenko <v.okhrimenko@gmail.com>
2021-03-03 00:15:14 +0200

    [HW-02] added overloaded  metod print in Main.java and update README.md

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a training repository
 2. just some text...
 3. added Main.java with main method with output to the console "Hello, world!"
 4. added a static print method that takes one parameter - string and that will be printed to the console
+5. add an overloaded print method with two parameters - string and number. The number corresponds to how many times you 
+need to print a string in the console
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
-        if (args.length == 1) {
-            print(args[0]);
-        } else {
-            System.out.println("Invalid number of arguments. Enter 2 arguments: first - text, second - how many " +
-                    "times text output to the console ");
+        switch (args.length) {
+            case 1:
+                print(args[0]);
+                break;
+            case 2:
+                try {
+                    print(args[0], Integer.parseInt(args[1]));
+                } catch (NumberFormatException e) {
+                    System.out.println("Enter correct second parameter - it must be digit(integer)");
+                }
+                break;
+            default:
+                System.out.println("Invalid number of arguments. Enter 2 arguments: first - text, second -integer. Its set how many " +
+                        "times text output to the console ");
         }
     }
 
     static void print(String txt) {
         System.out.println(txt);
+    }
+
+    static void print(String txt, int iteration) {
+        for (int i = 0; i < iteration; i++) {
+            System.out.println(txt);
+        }
     }
 }
